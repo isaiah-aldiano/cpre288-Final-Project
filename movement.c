2ce2919fb1f2a6124f5774d9824ffed2b7971d09
Lewis <lewisc65@iastate.edu>
2024-12-04 12:40:48 -0600

    sound

--- movement.c
+++ movement.c
@@ -17,6 +17,9 @@ void turn_clockwise(oi_t *sensor_data, int degrees);
 void bump (oi_t*sensor_data, int sum);
 void helpSend(char data[]);
 
+extern volatile  char uart_data;  // Your UART interupt code can place read data here
+extern volatile  char flag;       // Your UART interupt can update this flag
+
 void turn_clockwise(oi_t *sensor_data, int degrees){
     double sum = 0;
     oi_setWheels(-200, 200); // turn clockwise
@@ -54,7 +57,10 @@ void move_forward(oi_t*sensor_data, int centimeters){
         if(sensor_data->bumpLeft ||sensor_data->bumpRight){
             bump(sensor_data, sum);
             break;
-        }
+        } //if(sensor_data->cliffLeftSignal || sensor_data->cliffRightSignal){
+//            cliff(sensor_data, sum);
+//            break;
+//        }
          oi_update(sensor_data);
          sum += sensor_data->distance;
     }
@@ -62,20 +68,31 @@ void move_forward(oi_t*sensor_data, int centimeters){
 }
 
 void bump (oi_t*sensor_data, int sum){
-    char command = 't';
+    char uart_data = 't';
     oi_setWheels(0, 0);
 
+    //Send bump to GUI to alert user of object to remove
     char message[] = "Bump";
     helpSend(message);
 
-    while(command != 'c'){
-        command = uart_receive();
+    unsigned char notes[4] = {67, 67, 72, 67};
+    unsigned char duration[4] = {15, 15, 20, 15};
+    oi_loadSong(0, 4, notes, duration);
+
+    while(uart_data != 'c'){
+        //command = uart_receive();
+        oi_play_song(0);
     }
 
     move_forward(sensor_data, sum / 10);
 }
 
+//void cliff (){
+//
+//}
+
 void helpSend(char data[]){
+    uart_init(0);
     char i;
     for(i = 0; i < strlen(data); i++){
         uart_sendChar(data[i]);
